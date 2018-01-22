0ac97532d25852ad20df0243a9fa201670e45e88
rallysport206 <phan.tony125@gmail.com>
2018-01-21 20:15:53 -0800

    push portfolio idea

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//   $(document).foundation();
+//   $("#fullpage").fullpage({
+//     anchors: ["home", "about", "projects", "contact",],
+//     menu: ".navbar",
+//     slidesNavigation: true,
+//     css3: true,
+//     scrollingSpeed: 1000,
+//     responsiveWidth: 800,
+//     responsiveHeight: 300
+//   });
+// });
